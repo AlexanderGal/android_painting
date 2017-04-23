c84e66c6008b796247b39541f4c266c67da26ec2
Alexander <balron923@gmail.com>
2017-04-23 03:03:37 +0300

    add switch mode

--- app/src/main/java/com/sbthomework/painter/MainActivity.java
+++ app/src/main/java/com/sbthomework/painter/MainActivity.java
@@ -4,9 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
+    private TextView clearButton;
+    private TextView rectButton;
+    private TextView cirleButton;
+    private TextView lineButton;
+    private Switch modeSwitch;
     private PainterView painterView;
 
     @Override
@@ -14,14 +21,65 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.clear_button);
-        painterView = (PainterView) findViewById(R.id.paint_view);
+        layoutElementInit();
+        layoutElementsOnClickListners();
+    }
 
-        button.setOnClickListener(new View.OnClickListener() {
+    private void layoutElementsOnClickListners() {
+        modeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    painterView.setMode(PainterView.ERASE_MODE);
+                } else {
+                    painterView.setMode(PainterView.MULTY_MODE);
+                }
+            }
+        });
+
+        clearButton.setOnClickListener(new View.OnClickListener()
+
+        {
             @Override
             public void onClick(View v) {
                 painterView.clear();
             }
         });
+
+        rectButton.setOnClickListener(new View.OnClickListener()
+
+        {
+            @Override
+            public void onClick(View v) {
+                painterView.setCurrentCanvasType(PainterView.RECT_DRAW);
+            }
+        });
+
+        cirleButton.setOnClickListener(new View.OnClickListener()
+
+        {
+            @Override
+            public void onClick(View v) {
+                painterView.setCurrentCanvasType(PainterView.CIRCLE_DRAW);
+            }
+        });
+
+        lineButton.setOnClickListener(new View.OnClickListener()
+
+        {
+            @Override
+            public void onClick(View v) {
+                painterView.setCurrentCanvasType(PainterView.LINE_DRAW);
+            }
+        });
+    }
+
+    private void layoutElementInit() {
+        modeSwitch = (Switch) findViewById(R.id.switchMode);
+        clearButton = (TextView) findViewById(R.id.clear_button);
+        rectButton = (TextView) findViewById(R.id.choose_rect_button);
+        cirleButton = (TextView) findViewById(R.id.choose_circle_button);
+        lineButton = (TextView) findViewById(R.id.choose_line_button);
+        painterView = (PainterView) findViewById(R.id.paint_view);
     }
 }
--- app/src/main/java/com/sbthomework/painter/PainterView.java
+++ app/src/main/java/com/sbthomework/painter/PainterView.java
@@ -2,14 +2,16 @@ package com.sbthomework.painter;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Path;
+import android.graphics.PointF;
 import android.graphics.PorterDuff;
 import android.os.Build;
 import android.util.AttributeSet;
+import android.util.SparseArray;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -18,20 +20,24 @@ import android.view.View;
  */
 
 public class PainterView extends View {
+    public static final int LINE_DRAW = 0;
+    public static final int RECT_DRAW = 1;
+    public static final int CIRCLE_DRAW = 2;
+    public static final int MULTY_MODE = 0;
+    public static final int ERASE_MODE = 1;
+    private int currentMode = 0;
 
     private Bitmap mBitmap;
+
     private Canvas mBitmapCanvas;
-    Path path;
-
-
-    private float startX = 0.0f;
-    private float startY = 0.0f;
-
-    private float endX = 0.0f;
-    private float endY = 0.0f;
+    private Paint[] mPredefinedPaints;
+    private int mNextPaint = 0;
+    private PointF point;
 
     private Paint mEditModePaint = new Paint();
 
+    private int currentCanvasType;
+    private SparseArray<Paint> mPaints = new SparseArray<>(10);
 
     public PainterView(Context context) {
         super(context);
@@ -58,13 +64,18 @@ public class PainterView extends View {
         if (getRootView().isInEditMode()) {
             mEditModePaint.setColor(Color.MAGENTA);
         } else {
-            Paint paint = new Paint();
-            paint.setAntiAlias(true);
-            paint.setColor(getResources().getColor(R.color.colorPrimary));
-            paint.setStrokeCap(Paint.Cap.ROUND);
-            paint.setStrokeJoin(Paint.Join.ROUND);
-            paint.setStrokeWidth(getResources().getDimension(R.dimen.default_paint_width));
+            TypedArray ta = getResources().obtainTypedArray(R.array.paint_colors);
+            mPredefinedPaints = new Paint[ta.length()];
 
+            for (int i = 0; i < ta.length(); i++) {
+                Paint paint = new Paint();
+                paint.setAntiAlias(true);
+                paint.setColor(ta.getColor(i, 0));
+                paint.setStrokeCap(Paint.Cap.ROUND);
+                paint.setStrokeJoin(Paint.Join.ROUND);
+                paint.setStrokeWidth(getResources().getDimension(R.dimen.default_paint_width));
+                mPredefinedPaints[i] = paint;
+            }
         }
     }
 
@@ -86,30 +97,55 @@ public class PainterView extends View {
         }
     }
 
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
         switch (event.getActionMasked()) {
             case MotionEvent.ACTION_DOWN:
             case MotionEvent.ACTION_POINTER_DOWN:
-                startX = event.getX();
-                startY = event.getY();
+                int pointerId = event.getPointerId(event.getActionIndex());
+                point = new PointF(event.getX(event.getActionIndex()), event.getY(event.getActionIndex()));
+                mPaints.put(pointerId, mPredefinedPaints[mNextPaint % mPredefinedPaints.length]);
+                mNextPaint++;
                 return true;
             case MotionEvent.ACTION_MOVE:
+                Paint paint = mPaints.get(event.getPointerId(event.getActionIndex()));
 
-                mBitmapCanvas.drawOval(startX, startY, event.getX(), event.getY(), mEditModePaint);
-                mBitmapCanvas.save();
+                if (point != null) {
+                    float x = event.getX();
+                    float y = event.getY();
+                    drawCurrentType(point.x, point.y, x, y, paint);
+                }
                 invalidate();
                 return true;
             case MotionEvent.ACTION_POINTER_UP:
             case MotionEvent.ACTION_UP:
-//                mBitmapCanvas.drawOval(startX,startY,event.getX(),event.getY(),mEditModePaint);
-                invalidate();
                 return true;
         }
 
         return super.onTouchEvent(event);
+    }
+
+    private void drawCurrentType(float startX, float startY, float endX, float endY, Paint paint) {
+        if (currentCanvasType != 0 && currentMode == 1) {
+            clear();
+        }
+        switch (currentCanvasType) {
+            case RECT_DRAW:
+                mBitmapCanvas.drawRect(startX, startY, endX, endY, paint);
+                break;
+            case CIRCLE_DRAW:
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    mBitmapCanvas.drawOval(startX, startY, endX, endY, paint);
+                    break;
+                }
+            default:
+            case LINE_DRAW:
+                mBitmapCanvas.drawLine(startX, startY, endX, endY, paint);
+                point.x = endX;
+                point.y = endY;
+                break;
+        }
     }
 
     @Override
@@ -122,12 +158,18 @@ public class PainterView extends View {
         canvas.drawBitmap(mBitmap, 0, 0, null);
     }
 
-    /**
-     * Очищает нарисованное
-     */
     public void clear() {
         mBitmapCanvas.drawColor(Color.BLACK, PorterDuff.Mode.CLEAR);
         invalidate();
+    }
+
+    public void setCurrentCanvasType(int currentCanvasType) {
+
+        this.currentCanvasType = currentCanvasType;
+    }
+
+    public void setMode(int mode) {
+      currentMode = mode;
     }
 }
 
